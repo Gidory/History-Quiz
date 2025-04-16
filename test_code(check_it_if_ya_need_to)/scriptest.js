a82539b3bfbcc0eb6e14f5bf8c503fc511510ca5
Gidorin <153376115+Gidory@users.noreply.github.com>
2025-04-16 22:25:48 +0300

    Script Update
    Yeah, this is a pain. True pain. And it`s hurting my eyes as optimiser.

--- test_code(check_it_if_ya_need_to)/scriptest.js
+++ test_code(check_it_if_ya_need_to)/scriptest.js
@@ -4,7 +4,9 @@ let answ2 = document.querySelector("#answer2");
 let answ3 = document.querySelector("#answer3");
 let answ4 = document.querySelector("#answer4");
 let question = document.querySelector("#question");
-
+let questionnum = 0;
+let rumki = 0;
+let testnumber = 0;
 let Questions = [
 [
     "Who are You?",
@@ -19,5 +21,36 @@ let Questions = [
 
 ]
 ];
-let Answers = [[["An Idiot", "An Idiot", "An Idiot", "An Idiot"], [], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], [], []], 
+let Answers = [[["An Idiot1", "An Idiot2", "An Idiot3", "An Idiot4"], [], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], [], []], 
 [[], [], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], [], []]];
+let Correct = [[["An Idiot2", ], [], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], [], []], 
+[[], [], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], [], []]];
+
+let TetsInProgress = setInterval(function(){
+    TestProgress(questionnum, testnumber);
+}, 10);
+function TestProgress(i, b){
+    question.innerText = Questions[b][i];
+    answ1.innerText = Answers[b][i][0];
+    answ2.innerText = Answers[b][i][1];
+    answ3.innerText = Answers[b][i][2];
+    answ4.innerText = Answers[b][i][3];
+}
+function handleAnswerClick(answerText) {
+    const correctAnswer = Correct[testnumber][questionnum][0];
+    console.log(correctAnswer, answerText);
+    if (answerText === correctAnswer) {
+      rumki++;
+      console.log("correct");
+      console.log(rumki);
+    } else {
+      console.log("incorrect");
+    }
+  }
+  
+  // Event listeners
+  [answ1, answ2, answ3, answ4].forEach(button => {
+    button.addEventListener("click", () => {
+      handleAnswerClick(button.innerText);
+    });
+  });
