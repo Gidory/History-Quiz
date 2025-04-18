ba64bc6f26d6221bd16532dfc412b26578e47489
Gidorin <153376115+Gidory@users.noreply.github.com>
2025-04-18 23:07:20 +0300

    Useless update?
    It`s just one damn function

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ function importi(){
     sr.innerText = "Ваш результат: " + rumkis + "/10";
 }
 importi();
+let rm = document.querySelector('#resultcomm');
+function commenting(){
+    if(rumkis <=3){
+        rm.innerText = "Ну ти просто ЛОХ. Я не знаю навіть, що сказати, майже нічого не знаєте."
+    }
+    if(rumkis > 3&&rumkis <=6){
+        rm.innerText = "Щось знаєте, щось не знаєте. Раджу пошукати в Інтернеті відповіді на питання. Якщо ви їх ще пам'ятаєте звісно."
+    }
+    if(rumkis > 6&&rumkis <=9){
+        rm.innerText = "Чудова робота!"
+    }
+    if(rumkis > 9&&rumkis == 10){
+        rm.innerText = "Ідеально."
+    }
+}
+commenting();
